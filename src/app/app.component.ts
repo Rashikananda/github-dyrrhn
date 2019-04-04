778895fd07a9984b0413c62a25af064d975cf9dd
Rashikananda <rashikanandapattanaik@gmail.com>
2019-04-04 08:40:04 +0530

    k

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ declare const google: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  currentLat;
+  currentLong;
     constructor() {
     GoogleMapsLoader.KEY = 'AIzaSyBAyMH-A99yD5fHQPz7uzqk8glNJYGEqus';
   }
@@ -29,6 +31,39 @@ export class AppComponent implements OnInit {
       this.addMarker(event.latLng);
     });
     });
+    this.trackMe();
+  }
+
+  showPosition(position) {
+    console.log(position);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.markers) {
+      this.markers = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      // this.markers.setPosition(location);
+    }
+  }
+  trackMe() {
+    if (navigator.geolocation) {
+      // this.isTracking = true;
+      console.log("track")
+      navigator.geolocation.watchPosition((position) => {
+      console.log("")
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 
   private addMarker(location) {
